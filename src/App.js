6dbd7c1fd25ff05bbfaa894f207e044744cb7a1c
Matthew Holloway <matthew.holloway@logicwerx.com>
2020-12-20 19:30:21 -0500

    Add nav links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./App.css";
 import Routes from "./Routes";
+import  {LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
@@ -11,6 +13,16 @@ function App() {
           Scratch
         </Navbar.Brand>
         <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
