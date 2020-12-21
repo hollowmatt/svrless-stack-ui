b5f7199ddc70609e58e30a6bce16098a8ffd6131
Matthew Holloway <matthew.holloway@logicwerx.com>
2020-12-20 19:35:17 -0500

    Add a catch-all 'not found' route

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      {/* This next route needs to be the last one - catch all */}
+      <Route>
+          <NotFound />
       </Route>
     </Switch>
   );
