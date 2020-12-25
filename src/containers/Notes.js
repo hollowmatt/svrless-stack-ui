ad6d883f3e7f3796133e973a43f045f01f094548
Matthew Holloway <matthew.holloway@logicwerx.com>
2020-12-25 16:34:00 -0500

    Wired in the deletion API - file not deleted

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -85,6 +85,10 @@ export default function Notes() {
       }
   }
 
+  function deleteNote() {
+      return API.del("notes", `/notes/${id}`);
+  }
+
   async function handleDelete(event) {
       event.preventDefault();
 
@@ -95,6 +99,14 @@ export default function Notes() {
       }
 
       setIsDeleting(true);
+
+      try {
+          await deleteNote();
+          history.push("/");
+      } catch(e) {
+          onError(e);
+          setIsDeleting(false);
+      }
   }
 
   return (
